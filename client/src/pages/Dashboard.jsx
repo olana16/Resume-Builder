972b5a2e9029d8d980e354bc6c6295015153a576
olana16 <oligeda16@gmail.com>
2026-01-13 11:59:23 +0300

    start building dash board

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,36 @@
+import { PlugIcon, PlusIcon, UploadCloudIcon } from 'lucide-react';
 import React from 'react'
 
 const Dashboard = () => {
-  return (
-    <div>
-      <h1>Dashboard Page</h1>
-    </div>
-  )
+
+    return (
+        <div>
+            <div className='max-w-7xl mx-auto px-4 py-8'>
+                <p className='text-2xl font-medium mb-6 bg-linear-to-r
+             from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden'>Welcome Olana</p>
+
+                <div className='flex gap-4'>
+                    <button className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600
+                           border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer'>
+                        <PlusIcon className='size-11 transition-all duration-300 p-2.5 bg-linear-to-br
+                           from-indigo-300 to bg-indigo-500 text-white rounded-full' />
+                        <p className='text-sm group-hover:text-indigo-600 transition-all duration-300'>Create Resume</p>
+                    </button>
+
+                    <button className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600
+                           border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer'>
+                        <UploadCloudIcon className='size-11 transition-all duration-300 p-2.5 bg-linear-to-br
+                           from-purple-300 to bg-purple-500 text-white rounded-full' />
+                        <p className='text-sm group-hover:text-purple-600 transition-all duration-300'>Upload Existing </p>
+                    </button>
+
+                </div>
+
+                <hr className='border-slate-300 my-6 sm:w-[305px]' />
+            </div>
+
+        </div>
+    )
 }
 
 export default Dashboard;
