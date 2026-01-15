7c1200de2266fdb0f1e183263390fbd79bb57582
olana16 <oligeda16@gmail.com>
2026-01-15 10:15:57 +0300

    edit resume onclick functionality

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -40,6 +40,12 @@ const Dashboard = () => {
         navigate(`/app/builder/resum123`)
     }
 
+
+    const editTitle = async (event) => {
+        event.preventDefault();
+      //  setEditResumeId('');
+    }
+
     useEffect(() => {
         loadAllResumes()
     }, [])
@@ -86,12 +92,12 @@ const Dashboard = () => {
                                 <p className='absolute bottom-1 text-[11px] text-slate-400
                                  group-hover:text-slate-500 transition-all duration-300
                                   px-2 text-center' style={{ color: baseColors + '90' }}>Update on {new Date().toLocaleDateString()}</p>
-                                <div className='absolute top-1 right-1 group-hover:flex items-center hidden'>
-                                    <TrashIcon className='size-7 p-1 hover:bg-white/50 rounded text-slate-700 
-                                      transition-colors' />
-                                    <PencilIcon className='size-7 p-1 hover:bg-white/50 rounded text-slate-700 
-                                      transition-colors'/>
-                                </div>
+                                                                <div onClick={e => e.stopPropagation()} className='absolute top-1 right-1 group-hover:flex items-center hidden'>
+                                                                        <TrashIcon className='size-7 p-1 hover:bg-white/50 rounded text-slate-700 
+                                                                            transition-colors' />
+                                                                        <PencilIcon onClick={(e) => { e.stopPropagation(); setEditResumeId(resume._id); setTitle(resume.title); }} className='size-7 p-1 hover:bg-white/50 rounded text-slate-700 
+                                                                            transition-colors'/>
+                                                                </div>
                             </button>
                         )
 
@@ -164,6 +170,36 @@ const Dashboard = () => {
                         </form>
                     )
                 }
+
+           
+
+
+             
+                {
+                    editResumeId && (
+                        <form onSubmit={editTitle} onClick={() => setEditResumeId('')} className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center
+                        justify-center'>
+                            <div onClick={e => e.stopPropagation()} className='relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6'>
+                                <h2 className='text-xl font-bold mb-4'>Edit a Resume</h2>
+                                <input onChange={(e) => setTitle(e.target.value)} value={title} type="text" placeholder='Enter Resume title' className='w-full px-4 py-2 mb-4 
+                                focus:border-green-600 ring-green-600' required />
+
+                                <button className='w-full py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-colors'>
+                                    Update
+                                </button>
+
+                                <XIcon className='absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer
+                                 transition-colors' onClick={() => { setEditResumeId(''); setTitle('') }} />
+                            </div>
+
+                        </form>
+                    )
+                }
+
+
+
+
+
 
 
             </div>
