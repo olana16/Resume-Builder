51812b5960ff3774812b101c8197d26aa254ac25
olana16 <oligeda16@gmail.com>
2026-01-15 11:49:24 +0300

    continou resumebuillder page

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { dummyResumeData } from '../assets/assets';
-import { ArrowBigLeftIcon } from 'lucide-react';
+import { ArrowBigLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkles, User } from 'lucide-react';
 
 const ResumeBuilder = () => {
 
@@ -34,6 +34,22 @@ const loadExistingResume = async(resumeId) => {
   }
 }
 
+const [activeSectionIndex, setActiveSectionIndex] = useState(0);
+const [removeBackground, setRemoveBackground] = useState(false);
+
+const sections = [
+  
+  {id:"personal", name:"Personal Info", icon:User},
+  {id:"summary", name:"Summary", icon:FileText},
+  {id:"experience", name:"Experience", icon:Briefcase},
+  {id:"education", name:"Education", icon:GraduationCap},
+  {id:"projects", name:"Projects", icon:FolderIcon},
+  {id:"skills", name:"Skills", icon:Sparkles},
+
+]
+
+const activeSection = sections[activeSectionIndex];
+
 
 useEffect(() => {
   loadExistingResume()
@@ -51,6 +67,80 @@ useEffect(() => {
         <ArrowBigLeftIcon className='size-4' /> Back to Dashboard
 
         </Link>
+      </div>
+      <div className='max-w-7xl mx-auto px-4 pb-8'>
+         
+         <div className='grid lg:grid-cols-12 gap-8'>
+
+          {/* Left Panel - Form */}
+         <div className='relative lg:col-span-5 rounded-lg overflow-hidden'>
+
+          <div className='bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1'>
+            {/* progress bar using activeSectionIndex */}
+            <hr className='absolute top-0 left-0 right-0 border-2 border-gray-200'/>
+
+            <hr className='absolute top-0 left-0 h-1 bg-gradient-to-r from-green-500 to-green-600
+             border-none transition-all duration-200' style={{width:`${activeSectionIndex * 100 / (sections.length - 1)}%`}}/>
+
+             {/* section navigation */}
+
+             <div className='flex justify-between items-center mb-6 border-b border-gray-300 py-1'>
+
+              <div></div>
+
+              <div className='flex items-center'>
+                {
+                  activeSectionIndex !==0 && (
+                    <button onClick={()=> setActiveSectionIndex((prevIndex)=>Math.min(prevIndex +1, sections.length - 1))} className='flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50
+                     transition-all' disabled={activeSectionIndex===0}>
+
+                     <ChevronLeft  className='size-4'/>Previous
+
+                    </button>
+                  )
+                }
+                 
+                 <button onClick={()=> setActiveSectionIndex((prevIndex)=>Math.max(prevIndex -1,0))} 
+                 className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50
+                     transition-all ${activeSectionIndex=== sections.length - 1 && 'opacity-50'}`}  
+                     disabled={activeSectionIndex=== sections.length - 1}>
+
+                     Next <ChevronRight  className='size-4'/>
+
+                    </button>
+                  
+                
+
+              </div>
+
+             </div>
+
+             {/* Form Content */}
+             <div className='space-y-6'> 
+              { activeSection.id === 'personal' && (
+
+                <div> 
+
+                  
+                </div>
+
+              )
+              
+              }
+
+
+             </div>
+
+
+
+          </div>
+
+         </div>
+
+          {/* Right Panel - Resume Preview */}
+          <div></div>
+
+         </div>
       </div>
 
     </div>
