2b39fd6e0b9fea6d1e48cf5236fe5f0804f17dbe
olana16 <oligeda16@gmail.com>
2026-01-15 16:17:51 +0300

    Personal Information

--- client/src/components/PersonalInfoForm.jsx
+++ client/src/components/PersonalInfoForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { User } from 'lucide-react'
+import { BriefcaseBusiness, Globe, Linkedin, Mail, MapPin, Phone, User } from 'lucide-react'
 const PersonalInfoForm = ({data, onChange, removeBackground, setRemoveBackground}) => {
 
 
@@ -7,6 +7,18 @@ const PersonalInfoForm = ({data, onChange, removeBackground, setRemoveBackground
   const handleChange = (field, value) => {
     onChange({ ...data, [field]: value })
   }
+
+  const fields =[
+    {key: "full_name", label: "Full Name", icon:User, type: "text", required: true},
+    {key: "email", label: "Email", icon:Mail, type: "email", required: true},
+    {key: "phone", label: "Phone", icon: Phone, type: "tel", },
+    {key:"location", label: "Location", icon: MapPin, type: "text", },
+    {key:"profession", label: "Profession", icon: BriefcaseBusiness, type: "text",},
+    {key:"linkedIn", label: "LinkedIn Profile", icon: Linkedin, type: "url",},
+    {key:"website", label: "Professional Website", icon: Globe, type: "url",},
+
+  ]
+
   return (
     <div>
       <h2 className='text-lg font-semibold text-gray-900'>Personal Information</h2>
@@ -31,7 +43,7 @@ const PersonalInfoForm = ({data, onChange, removeBackground, setRemoveBackground
         </label>
        
         
-        {typeof data.image && (
+        { data.image && (
           <div className='flex flex-col gap-1 pl-4 text-sm'> 
            <p>Remove Background</p>
 
@@ -55,6 +67,33 @@ const PersonalInfoForm = ({data, onChange, removeBackground, setRemoveBackground
 
 
       </div>
+{
+  fields.map((field)=>{
+    const Icon = field.icon
+    return (
+      <div key={field.key} className="space-y-1 mt-5">
+        <label className='flex items-center gap-2 text-sm font-medium text-gray-600'>
+<Icon className='size-4' />
+          {field.label}
+{ field.required && <span className='text-red-500'>*</span> 
+
+}
+        </label>
+        <input type={field.type} value={data[field.key] || ''} onChange={(e)=>handleChange(field.key, e.target.value)}
+         required={field.required} className='mt-1 w-full px-3 py-2 border
+          border-gray-300 rounded-lg focus:ring focus:ring-blue-500
+          focus:border-blue-500 outline-none transition-colors text-sm' placeholder={`Enter your ${field.label.toLowerCase()}`}/>
+
+
+    
+
+
+      </div>
+    )
+  })
+}
+
+
     </div>
   )
 }
