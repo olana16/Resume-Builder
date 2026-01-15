4c8d56878e1001b2045bea0c18ae3781cf9db338
olana16 <oligeda16@gmail.com>
2026-01-15 12:03:09 +0300

    personal info component

---
+++ client/src/components/PersonalInfoForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PersonalInfoForm = ({data, onChange, removeBackground, setRemoveBackground}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PersonalInfoForm
