74d5863bc4b1cd8beb6f7d556a45a753b84440ee
olana16 <oligeda16@gmail.com>
2026-01-17 14:43:25 +0300

    project form function created

--- client/src/components/ProjectForm.jsx
+++ client/src/components/ProjectForm.jsx
@@ -1,6 +1,30 @@
 import React from 'react'
 
 const ProjectForm = ({data, onChange}) => {
+
+     const addProject = () => {
+        const newProject = {
+            institution: "",
+            degree: "",
+            field: "",
+            graduation_date: "",
+            gpa: "",
+        }
+        onChange([...data, newProject])
+    }
+
+    const removeProject = (index) => {
+        onChange(data.filter((_, i) => i !== index))
+    }
+
+    const updateProject = (index, field, value) => {
+        const updated = [...data]
+        updated[index] = { ...updated[index], [field]: value }
+        onChange(updated)
+    }
+
+
+
   return (
     <div>
       
