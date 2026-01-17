b425d52cee39eddcca5c5964aee308835a78838f
olana16 <oligeda16@gmail.com>
2026-01-17 15:00:48 +0300

    Project detail UI completed

--- client/src/components/ProjectForm.jsx
+++ client/src/components/ProjectForm.jsx
@@ -1,3 +1,4 @@
+import { Plus, Trash2 } from 'lucide-react'
 import React from 'react'
 
 const ProjectForm = ({data, onChange}) => {
@@ -24,9 +25,55 @@ const ProjectForm = ({data, onChange}) => {
 
 
   return (
-    <div>
-      
-    </div>
+   <div>
+
+            <div className='flex items-center justify-between'>
+                <div>
+                    <h3 className='flex items-center justify-center gap-2 text-lg font-semibold text-gray-900'>Projects</h3>
+                    <p className='text-sm text-gray-500'>Add your Projects </p>
+                </div>
+                <button onClick={addProject} className='flex items-center gap-2 px-3 py-1 text-sm
+                 bg-green-100 text-green-700 rounded-lg hover:bg-green-200 transition-colors'>
+                    <Plus className='size-4' />
+                    Add Projects
+                </button>
+            </div>
+
+                <div className='space-y-4 mt-6'>
+                    {data.map((project, index) => (
+                        <div key={index} className='p-4 border border-gray-200 rounded-lg space-y-3'>
+                            <div className='flex justify-between items-start'>
+                                <h4>Project #{index + 1}</h4>
+                                <button onClick={() => removeProject(index)} className='text-red-500 hover:text-red-700 transition-colors'>
+                                    <Trash2 className='size-4' />
+                                </button>
+                            </div>
+
+                            <div className='grid md:grid-cols-2 gap-3'>
+
+                                <input value={project.name || ""} onChange={(e) => updateProject(index, "name", e.target.value)} className='px-3 py-2 text-sm rounded-lg' type="text" placeholder='Project Name' />
+                                
+                                
+                                <input value={project.type || ""} onChange={(e) => 
+                                            updateProject(index, "type", e.target.value)} 
+                                            className='px-3 py-2 text-sm rounded-lg' type="text" placeholder='Project Type' />
+
+
+                               <textarea  rows={4}
+                                value={project.description || ""} onChange={(e) => 
+                                            updateProject(index, "description", e.target.value)} 
+                                            className ='w-full px-3 py-2 text-sm rounded-lg resize-none' type="text" placeholder='Project Description' />
+ 
+
+                            </div>
+                                                           
+                        </div>
+                    ))}
+
+                </div>
+            
+
+        </div>
   )
 }
 
