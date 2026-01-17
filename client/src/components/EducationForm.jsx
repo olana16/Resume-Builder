67f6052eb568bdc0740202132766ceacd28cb662
olana16 <oligeda16@gmail.com>
2026-01-17 12:01:32 +0300

    function for education form created

--- client/src/components/EducationForm.jsx
+++ client/src/components/EducationForm.jsx
@@ -1,6 +1,32 @@
 import React from 'react'
 
-const EducationForm = () => {
+const EducationForm = ({data, onChange}) => {
+
+
+
+      const addEducation = () => {
+        const newEducation = {
+            institution: "",
+            degree: "",
+            field: "",
+            graduation_date: "",
+            gpa: "",
+        }
+        onChange([...data, newExperience])
+    }
+
+    const removeEducation = (index) => {
+        onChange(data.filter((_, i) => i !== index))
+    }
+
+    const updateEducation = (index, field, value) => {
+        const updated = [...data]
+        updated[index] = { ...updated[index], [field]: value }
+        onChange(updated)
+    }
+
+
+
   return (
     <div>
       
