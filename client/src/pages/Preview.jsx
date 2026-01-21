f2b7bbceb8cb506b061ebef49cc5b6da5365fcd2
olana16 <oligeda16@gmail.com>
2026-01-21 14:09:07 +0300

    UI Completed

--- client/src/pages/Preview.jsx
+++ client/src/pages/Preview.jsx
@@ -40,7 +40,7 @@ const Preview = () => {
 
           <div className='flex flex-col items-center justify-center h-screen'>
             <p className='text-center text-6xl text-slate-400 font-medium'>Resume not found</p>
-            <a href="" className='mt-6 bg-green-500 hover:bg-green-600
+            <a href="/" className='mt-6 bg-green-500 hover:bg-green-600
              text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 
               ring-green-400 flex items-center transition-colors'>
               <ArrowLeftIcon className='mr-2 size-4' /> Go to home page
