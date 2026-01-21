f1be020557e7a643b752ed75a93b2e8187a45244
olana16 <oligeda16@gmail.com>
2026-01-21 11:35:41 +0300

    function for resumePreview

--- client/src/pages/Preview.jsx
+++ client/src/pages/Preview.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyResumeData } from '../assets/assets';
 
 const Preview = () => {
+
+  const {resumeId} = useParams();
+  const [resumeData, setResumeData] = useState(null);
+
+  const loadResume =   () =>{
+    // Logic to load resume data based on resumeId
+    setResumeData(dummyResumeData.find(resume => resume._id === resumeId || null));
+
+  }
+ 
+  useEffect(()=>{
+    loadResume();
+    
+  },[])
+
   return (
     <div>
       <h1>Preview Page</h1>
