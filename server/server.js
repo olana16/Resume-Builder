c40b48d35ab2d05a8747bfe4f75b018da497072b
olana16 <oligeda16@gmail.com>
2026-01-21 15:17:05 +0300

    mongodb connected succssesfully

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import "dotenv/config";
+import connectDB from './configs/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Database connection
+await connectDB()
+
+// Middleware
 app.use(express.json());
 app.use(cors());
 
