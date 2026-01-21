97e13eea810c885d710b5657d97e885ddb5544e8
olana16 <oligeda16@gmail.com>
2026-01-21 11:57:21 +0300

    resume preview and loader created

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div>
+    <div className='flex items-center justify-center h-screen'>
+        <div className='size-12 border-3 border-green-400 border-t-transparent 
+        rounded-full animate-spin'>
+
+        </div>
       
     </div>
   )
