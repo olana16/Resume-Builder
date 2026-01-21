af09342683822e6982e7004fceef88f523423a05
olana16 <oligeda16@gmail.com>
2026-01-21 10:44:17 +0300

    private public and download button

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { dummyResumeData } from '../assets/assets';
-import { ArrowBigLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkles, User } from 'lucide-react';
+import { ArrowBigLeftIcon, Briefcase, ChevronLeft, ChevronRight, DownloadIcon, EyeIcon, EyeOffIcon, FileText, FolderIcon, GraduationCap, Share2Icon, Sparkles, User } from 'lucide-react';
 import PersonalInfoForm from '../components/PersonalInfoForm';
 import ResumePreview from '../components/ResumePreview';
 import TemplateSelector from '../components/TemplateSelector';
@@ -212,9 +212,34 @@ const ResumeBuilder = () => {
 
           <div className='lg:col-span-7 max-lg:mt-6'>
 
-            <div>
+            <div className='relative w-full'>
+              <div className='absolute bottom-3 left-0 right-0 flex items-center justify-end gap-2'>
+                {
+                  resumeData.public && (
+                    <button className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br
+                     from-blue-100 to-blue-200 text-blue-600 rounded-lg ring-blue-300
+                      hover:ring transitions-colors'>
+                      <Share2Icon className='size-4'/>
+                    </button>
+                  )
+                }
 
-                          {/* buttons */}
+                <button className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-purple-100
+                 to-purple-200 text-purple-600 ring-purple-300 rounded-lg hover:ring transition-colors'>
+                  {resumeData.public ? <EyeIcon className='size-4'/> : 
+                  <EyeOffIcon className='size-4'/>}
+                  {resumeData.public ? 'Public' : 'Private'}
+
+                </button>
+
+                <button className='flex items-center gap-2 px-6 py-2 text-xs bg-gradient-to-br from-green-100
+                 to-green-200 text-green-600 rounded-lg ring-green-300 hover:ring transition-colors'>
+                  <DownloadIcon className='size-4'/>Download
+
+                </button>
+
+              </div>
+
 
 
             </div>
