d81dd6e6dae7c73eb9e64efb7f32245648b5d34b
olana16 <oligeda16@gmail.com>
2026-01-22 10:10:46 +0300

    get resume by id created

--- server/controllers/resumeControllers.js
+++ server/controllers/resumeControllers.js
@@ -38,10 +38,34 @@ export const deleteResume = async(req, res) =>{
         const userId = req.userId;
         const { resumeId } = req.params;
 
-        await Resume.findOneAndDelete({userId: userId, _id: resumeId, user: userId });
+        await Resume.findOneAndDelete({userId, _id: resumeId, user: userId });
         return res.status(200).json({ message: "Resume deleted successfully" });
     }catch (error) {
 
         return res.status(400).json({ message: "Resume deletion failed", error: error.message });
 }
+}
+
+//get resume by id controller
+//GET /api/resumes/get
+
+export const getResumeById = async(req, res) =>{
+    try{
+        const userId = req.userId;
+        const { resumeId } = req.params;
+
+        const resume = await Resume.findOne({userId, _id: resumeId });
+        if(!resume){
+            return res.status(404).json({ message: "Resume not found" });
+        }
+
+        resume.__v = undefined; // hide __v field
+        resume.createdAt = undefined; // hide createdAt field
+        resume.updatedAt = undefined; // hide updatedAt field
+
+        return res.status(200).json({ resume });
+
+    }catch (error) {
+        return res.status(400).json({ message: "Failed to fetch resume", error: error.message });
+    }
 }
