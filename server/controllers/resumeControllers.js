efe1822e859a9edaa5f989e44ff1a716443b6fb0
olana16 <oligeda16@gmail.com>
2026-01-22 11:01:23 +0300

    image kit integration

--- server/controllers/resumeControllers.js
+++ server/controllers/resumeControllers.js
@@ -3,7 +3,9 @@
 
 // Controllers for creating resume
 
+import imageKit from "../configs/imageKit.js";
 import Resume from "../models/Resume.js";
+import fs from "fs";
 
 //GET /api/resumes/create
 export const createResume = async(req, res) =>{
@@ -101,6 +103,27 @@ export const updateResume = async(req, res) =>{
 
 
         let resumeDataCopy = JSON.parse(resumeData);
+
+        if(image){
+
+            const imageBufferData = fs.createReadStream(image.path);
+            
+
+            const response = await imageKit.files.upload({
+               file: imageBufferData,
+                 fileName: 'resume.png',
+                 folder: 'user-resumes',
+                 transformation:{
+                    pre:'w-300, h-300, fo-face, z-0.75,'+ (removeBackground ? 'e-bgremove,' : '');
+                 }
+
+                 });
+
+
+                 resumeDataCopy.personal_info.image = response.url;
+
+
+        }
 
      const resume =   await Resume.findOneAndUpdate(
             { userId, _id: resumeId },
