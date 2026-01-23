a48fdbfee5db8379ddbb18d5d1159b5bd6777957
olana16 <oligeda16@gmail.com>
2026-01-23 10:51:50 +0300

    ai enhance for upload resume created

--- server/controllers/aiControllers.js
+++ server/controllers/aiControllers.js
@@ -3,6 +3,8 @@
 //controllers for enhance professional ai features summary
 //POST /api/ai/enhance-pro-sum
 
+import Resume from "../models/Resume.js";
+
 
 export const enhanceProfessionalSummary = async(req, res) =>{
 
@@ -73,5 +75,101 @@ export const enhanceJobDescription = async(req, res) =>{
 
     }catch (error) {
         return res.status(400).json({ message: "Failed to enhance job description", error: error.message });
+    }
+}
+
+
+//controllers for uploading a resume to the database
+//POST /api/ai/upload-resume
+
+
+export const uploadResume = async(req, res) =>{
+    try {
+        const { resumeText, title } = req.body;
+        const userId = req.userId;
+
+        if(!resumeText){
+            return res.status(400).json({ message: "Resume text are required" });
+        }
+
+
+
+
+        const systemPrompt = "You are an expert AI agent to extract data from resume"
+
+        const userPrompt = `Extraxt data from this resume: ${resumeText} 
+
+     provide data in the following JSON format with no addational text before or after:
+
+     {
+
+      professional_summary: { type: String, default: "" },
+    skills:[{ type: String }],
+    personal_info:{
+        image: { type: String, default: "" },
+        full_name: { type: String, default: "" },
+        procession: { type: String, default: "" },
+        email: { type: String, default: "" },
+        phone: { type: String, default: "" },
+        location: { type: String, default: "" },
+        linkedin: { type: String, default: "" },
+        website: { type: String, default: "" },
+    },
+
+
+    experience:[{
+        company: { type: String, default: "" },
+        position: { type: String, default: "" },
+        start_date: { type: String, default: "" },
+        end_date: { type: String, default: "" },
+        description: { type: String, default: "" },
+        is_current: { type: Boolean, default: false },
+    }],
+
+    project: [{
+        name: { type: String, default: "" },
+        type: { type: String, default: "" },
+        description: { type: String, default: "" },
+    }],
+
+    education: [{
+        institution: { type: String, default: "" },
+        degree: { type: String, default: "" },
+        field: { type: String, default: "" },
+        graduation_date: { type: String, default: "" },
+        gpa: { type: String, default: "" },
+    }],
+
+     
+      }
+        `
+
+
+          const response = await ai.chat.completions.create({
+            model: process.env.OPENAI_MODEL,
+            messages: [
+                {
+                    role: "system",
+                    content: systemPrompt
+                },
+                {
+                    role: "user",
+                    content: userPrompt
+                }
+            ],
+            response_format: {type: "json_object"}
+        })
+
+        const extractedData = response.choices[0].message.content; 
+        const parsedData = JSON.parse(extractedData);
+        const newResume = await Resume.create({
+            userId,title,...parsedData
+        })
+        res.json({resumeId: newResume._id})
+        
+
+    } catch (error) {
+        return res.status(400).json({ message: "Failed to upload resume", error: error.message });
+        
     }
 }
