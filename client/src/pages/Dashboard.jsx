3195224d7589b93277570b63da386d50d0cb5b47
olana16 <oligeda16@gmail.com>
2026-01-24 10:48:34 +0300

    create resume completed

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,9 +2,14 @@ import { FilePenLineIcon, PencilIcon, PlugIcon, PlusIcon, TrashIcon, UploadCloud
 import React, { useState, useEffect, use } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { dummyResumeData } from '../assets/assets'
+import { useSelector } from 'react-redux';
+import api from '../configs/api';
+import toast from 'react-hot-toast';
 
 
 const Dashboard = () => {
+
+    const {user, token} = useSelector((state) => state.auth);
 
     const colors = ["#9333ea", "#d97706", "#dc2626", "#0284c7", "#16a34a"]
 
@@ -27,10 +32,20 @@ const Dashboard = () => {
     }
 
     const createResume = async (event) => {
-        event.preventDefault();
-        setShowCreateResume(false);
-        navigate(`/app/builder/resum123`)
+        
+        try {
+            event.preventDefault();
+            const {data}=await api.post('/api/resumes/create', {title}, {headers: {Authorization: token}});
+            setAllResumes([...allResumes, data.resume]);
+            setTitle('');
+            setShowCreateResume(false);
+            navigate(`/app/builder/${data.resume._id}`)
 
+
+        } catch (error) {
+            toast.error(error?.response?.data?.message || error.message);
+            
+        }
 
     }
 
