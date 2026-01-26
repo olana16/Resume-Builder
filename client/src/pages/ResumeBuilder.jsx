5e2814deba472b324cb04244739d9f1c49be34e9
olana16 <oligeda16@gmail.com>
2026-01-26 11:46:57 +0300

    save button link with functionality

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -278,7 +278,7 @@ try {
 
               </div>
 
-              <button className='bg-linear-to-br from-green-100 to-green-200 ring-green-300 text-green-600
+              <button onClick={toast.promise(saveResume, {loading: 'Saving...'})} className='bg-linear-to-br from-green-100 to-green-200 ring-green-300 text-green-600
                 ring hover:ring-green-400 transition-all rounded-md py-2 px-6 mt-6 text-sm'>
                 Save Resume
               </button>
